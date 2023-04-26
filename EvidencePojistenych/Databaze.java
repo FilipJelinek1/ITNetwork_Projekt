a094258e164445da44c9bb1a1300b8955c2e32e4
FilipJelinek1 <jelinek.filip@seznam.cz>
2023-04-26 14:12:34 +0200

    Update Databaze.java

--- EvidencePojistenych/Databaze.java
+++ EvidencePojistenych/Databaze.java
@@ -3,6 +3,9 @@ package EvidencePojistenych;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/*
+@Author Filip
+ */
 public class Databaze {
 
 
@@ -14,6 +17,7 @@ public class Databaze {
         databazePojistencu = new ArrayList<>();
     }
 
+    // přidání nového pojištěnce
     public void pridejPojisteneho(String jmeno, String prijmeni, int vek, int telefon) {
 
         System.out.println("jméno: " + jmeno);
@@ -21,26 +25,24 @@ public class Databaze {
         System.out.println("věk: " + vek);
         System.out.println("telefon: " + telefon);
         databazePojistencu.add(new Pojistenci(jmeno, prijmeni, vek, telefon));
-
     }
 
+   // výpis pojištěného
     public ArrayList<Pojistenci> vypisPojistence(String jmeno, String prijmeni) {
         ArrayList<Pojistenci> nalezene = new ArrayList<>();
 
         for (Pojistenci user : databazePojistencu) {
             {
-                if(pojistenci.getJmeno().equals(jmeno)
-                        && pojistenci.getPrijmeni().equals(prijmeni)) {
+                if (user.getJmeno().equalsIgnoreCase(jmeno) || user.getPrijmeni().equalsIgnoreCase(prijmeni)) {
                     System.out.println("nalezen pojištěnec dle požadavků");
                     nalezene.add(user);
                 }
-
             }
         }
         System.err.println("nenalezen žádný pojištěnec dle požadavků");
         return nalezene;
     }
-
+    // výpis všech záznamů v databázi
     public ArrayList<Pojistenci> vypisVsechPojistencu() {
         return databazePojistencu;
     }
